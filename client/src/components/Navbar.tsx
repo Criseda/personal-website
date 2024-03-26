6714436306f2a930243b949b1051f4c15559ced6
criseda <laurentiu.cristian.preda@gmail.com>
2024-03-26 00:41:45 +0000

    Navbar v2 implementation.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,37 +1,59 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-//   NavigationMenuIndicator,
+  // NavigationMenuContent,
+  // NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-//   NavigationMenuViewport,
+  // NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  // NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
-import { DarkModeToggle } from "./ui/dark-mode-toggle";
+import { DarkModeToggle } from "@/components/ui/dark-mode-toggle";
+import { MyAvatar } from "@/components/MyAvatar";
 
 export function Navbar() {
   return (
-      <div className="flex justify-between">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div className="m-2 mr-3">
-          <DarkModeToggle />
+    <div className="grid grid-cols-3 gap-4">
+      <div className="flex items-center ml-2">
+        <figure className="mr-3">
+          <MyAvatar />
+        </figure>
+        <div className="hidden sm:flex">
+          <h1 className="text-base font-semibold">Cristian Preda</h1>
         </div>
       </div>
+
+        <div className="flex justify-center">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/about">
+                  About
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/projects">
+                  Projects
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/skills">
+                  Skills
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/contacts">
+                  Contact
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+      <div className="flex justify-end m-2 mr-3">
+        <DarkModeToggle />
+      </div>
+    </div>
   );
 }
