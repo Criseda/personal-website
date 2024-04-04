23f8504371444871cec3c2c5abcf8de432160f9d
criseda <laurentiu.cristian.preda@gmail.com>
2024-04-04 15:10:50 +0100

    Repositioned divs across the navbar, using padding instead of margin

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,44 +14,56 @@ import { MyAvatar } from "@/components/MyAvatar";
 
 export function Navbar() {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="flex items-center ml-2">
-        <figure className="mr-3">
+    // <div className="grid grid-cols-3 gap-4">
+    <div className="relative flex justify-between items-center">
+      <div className="flex items-center">
+        <figure className="px-3">
           <MyAvatar />
         </figure>
-        <div className="hidden sm:flex">
+        <div className="hidden md:flex">
           <h1 className="text-base font-semibold">Cristian Preda</h1>
         </div>
       </div>
-
-        <div className="flex justify-center">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/about">
-                  About
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/projects">
-                  Projects
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/skills">
-                  Skills
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/contacts">
-                  Contact
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-
-      <div className="flex justify-end m-2 mr-3">
+      <div className="absolute left-1/2 transform -translate-x-1/2">
+        <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              href="/about"
+            >
+              About
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              href="/projects"
+            >
+              Projects
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              href="/skills"
+            >
+              Skills
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              href="/contacts"
+            >
+              Contact
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      </div>
+      
+      <div className="p-2 pr-3">
         <DarkModeToggle />
       </div>
     </div>
