c76d38747cea77e091dbf0c3ea20e5e975eba266
criseda <laurentiu.cristian.preda@gmail.com>
2024-07-08 11:47:11 +0100

    added temp text for layout visualisation

--- client/src/components/routes/Contact.tsx
+++ client/src/components/routes/Contact.tsx
@@ -4,6 +4,16 @@ export default function Contact() {
           <div className="text-2xl md:text-5xl font-bold dark:text-white text-left">
             Contact
           </div>
+          <div>
+            <p className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+              I am a full stack developer with a passion for creating beautiful and functional web applications. I have experience with a variety of technologies including React, Node.js, and MongoDB. I am currently a student at the University of Manchester studying Computer Science. I am always looking for new opportunities to learn and grow as a developer.
+            </p>
+          </div>
+          <div>
+            <p className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+              In my free time, I enjoy playing video games, reading, and working on personal projects. I am also a big fan of anime and manga.
+            </p>
+          </div>
         </div>
     );
 }
